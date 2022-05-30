861ace025848c69f418fc8060b640e288fb80d75
howsad <howshad@gmail.com>
2022-05-30 11:40:23 +0300

    Initial commit.

---
+++ src/main/java/ru/yandex/interview/courier/partialreturn/PartialReturnApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.interview.courier.partialreturn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartialReturnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartialReturnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yandex/interview/courier/partialreturn/PartialreturnApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.interview.courier.partialreturn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartialreturnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
